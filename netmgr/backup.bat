72e8c3694d0f0d3efd77d6af1ae66216118b7155
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:16:15 +1100

    Initial import

---
+++ netmgr/backup.bat
@@ -0,0 +1,11 @@
+del c:\timed\backnet.zip
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\*.h
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\*.c
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\os2\makefile.*
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\os2\*.lnk
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\dos\makefile.*
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\dos\*.lnk
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\d386\makefile.*
+pkzip2 -ex -P c:\timed\backnet.zip d:\tc\netmgr\d386\*.lnk
+copy c:\timed\backnet.zip f:\safe\backnet.zip
+
