a1c0b40daa41f61fcb0503db13207be0ecd3eb6b
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:16:07 +1100

    Initial import

---
+++ msgapi/months.c
@@ -0,0 +1,10 @@
+
+#ifndef __WATCOMC__
+char months[][10]={"January","February","March","April","May",
+									 "June","July","August","September","October",
+									 "November","December"};
+#endif
+
+char months_ab[][4]={"Jan","Feb","Mar","Apr","May","Jun","Jul",
+										 "Aug","Sep","Oct","Nov","Dec"};
+
