6f9e5e0283bd15a8126cd450b8a9ef61b8b320c9
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:16:14 +1100

    Initial import

---
+++ netmgr/reply.cmd
@@ -0,0 +1,11 @@
+@echo off
+echo Automatic Reply, @myaka, *, *, Response to your message, * >> %1
+echo Hello %%ffrom! >> %1
+echo. >> %1
+echo This is an automatic reply! >> %1
+echo. >> %1
+echo The subject was: %2 >> %1
+echo. >> %1
+echo Greetings! >> %1
+pause
+
