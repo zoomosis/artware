9c87aa7363b4c4efa5e17f7f0375091cdcacc327
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:16:38 +1100

    Initial import

---
+++ timed/normos2c.c
@@ -0,0 +1,19 @@
+
+
+void pascal normalize(char *s)
+{
+    char   *tmp = s;
+
+    while (*s)
+        {
+        if (*s == 0x8d)
+            s++;
+        else if (*s == '\n')
+            s++;
+        else
+            *tmp++ = *s++;
+        }
+
+    *tmp = '\0';
+}
+
