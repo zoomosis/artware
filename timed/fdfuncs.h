687983b26a7d2a10ac7f5fab8c92f36a0133de69
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:16:39 +1100

    Initial import

---
+++ timed/fdfuncs.h
@@ -0,0 +1,18 @@
+
+int FDinit(int syslookup);
+int FDclose(void);
+short int FDcurr(ADDRLIST *found);
+short int FDhead(ADDRLIST *found);
+short int FDtail(ADDRLIST *found);
+short int FDmark(void);
+short int FDfindmark(ADDRLIST *found);
+short int FDsearch_name(char *name, ADDRLIST *found);
+short int FDsearch_node(NETADDR *address, ADDRLIST *found);
+int get_node(unsigned blockno, void * node, int size);
+int FDnextname(void);
+int FDnextnode(void);
+int FDprevname(void);
+int FDprevnode(void);
+
+void FDupper(char *in);
+
