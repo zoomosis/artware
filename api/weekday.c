1ee84650194e3a15156a4a5748a2043ce61f314a
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:15:02 +1100

    Initial import

---
+++ api/weekday.c
@@ -0,0 +1,6 @@
+#ifndef __WATCOMC__
+char weekday[][10]={"Sunday","Monday","Tuesday","Wednesday","Thursday",
+                    "Friday","Saturday"};
+#endif
+char weekday_ab[][4]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+
