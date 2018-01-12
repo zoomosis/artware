9bf7d24cfd489451605b9bdcde053c2f16d08f8e
Andrew Clarke <mail@ozzmosis.com>
2018-01-13 06:15:02 +1100

    Initial import

---
+++ api/dr.h
@@ -0,0 +1,22 @@
+
+/* For chdir(), getcurdir(), setdisk(), getdisk() etc. calls */
+
+#include "ffind.h"
+
+#ifdef __TURBOC__
+
+#include <dir.h>
+
+#define _A_SUBDIR MSDOS_SUBDIR
+#define _A_SYSTEM MSDOS_SYSTEM
+
+#else
+
+#ifdef __GNUC__
+	#include <dirent.h>
+#else
+	#include <direct.h>
+#endif
+#endif
+
+
