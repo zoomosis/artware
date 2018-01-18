e792c2e3c266f4ecfde5aea88b9d4f212395c4a4
Andrew Clarke <mail@ozzmosis.com>
2018-01-18 21:35:22 +1100

    Added parameter

--- timed/utils/vidtest.c
+++ timed/utils/vidtest.c
@@ -9,19 +9,19 @@ void main(void)
 
    cls();
 
-   biprint(1,0,7,14,"Press ~a key~ to continue..");
+   biprint(1,0,7,14,"Press ~a key~ to continue..", ' ');
    getch(); cls();
 
    for(l=0; l<20; l++)
      {
-     biprinteol(l+2,l,112,126,"Dit is een ~mooie~ regel! ");
+     biprinteol(l+2,l,112,126,"Dit is een ~mooie~ regel! ", ' ');
      }
 
-   biprinteol(18,0,113,126,"Wat heeft die kerel een ~grote~ zeg!");
+   biprinteol(18,0,113,126,"Wat heeft die kerel een ~grote~ zeg!", ' ');
 
    getch();
    cls();
-   biprint(10,10,11,30,"ีอ~ test ~ออออออออออออออออออออออธ");
+   biprint(10,10,11,30,"ีอ~ test ~ออออออออออออออออออออออธ", ' ');
    print(11,10,11,"ณ                             ณ");
    print(12,10,11,"ณ                             ณ");
    print(13,10,11,"ณ                             ณ");
