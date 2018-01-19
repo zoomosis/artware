c56515424fea53874fd47078f165bfaf959e3350
Andrew Clarke <mail@ozzmosis.com>
2018-01-19 23:04:30 +1100

    Remote registration junk

--- netmgr/nstruct.h
+++ netmgr/nstruct.h
@@ -4,43 +4,6 @@
 
 #define NETMGRCODE 0xAEAE5656L
 
-typedef struct
-{
-   char   rand1[12];
-   dword  generated;
-   char   rand2[10];
-   dword  keyval;
-   char   rand3[7];
-   dword  ascvals;
-   dword  crc2;
-   char   regsite;
-   word   serial;
-   dword  ascvals2;
-   dword  ascvals3;
-   char   rand4[17];
-   word   ascvals4;
-   word   zero;
-   char   rand5[14];
-   dword  total;
-   char   rand6[33];
-
-} STRKEY;
-
-
-typedef struct
-{
-
-   char   bytes[128];
-
-} ARRKEY;
-
-
-union KEY
-{
-   STRKEY strkey;
-   ARRKEY arrkey;
-};
-
 #define REGISTERED (cfg.registered == 1)
 
 // ====================================================
@@ -238,7 +201,6 @@ typedef struct
    int       frodolog;
    int       intlforce;
    NETADDR   homeaddress[tMAXAKAS];
-   union KEY key;
    MASKLIST  *firstmask;
    char      registername[36];
    char      homedir[128];
