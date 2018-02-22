1a1a97907734657eedef103f1bd9a86ca7663305
Andrew Clarke <mail@ozzmosis.com>
2018-02-22 13:28:30 +1100

    Add missing files to distfiles

--- timed/dist.sh
+++ timed/dist.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-COMMON="../COPYING doc/chrstuff.zip doc/advanced.cfg doc/timed.cfg doc/readme.txt doc/changes.txt doc/timed.txt"
+COMMON1="../COPYING doc/chrstuff.zip doc/advanced.cfg doc/timed.cfg doc/timkeys.cfg"
+COMMON2="doc/readmaps.dat doc/writmaps.dat doc/readme.txt doc/changes.txt doc/timed.txt"
+
 ICONSW="doc/icons/windows/timed.ico doc/icons/windows/timed2.ico"
 ICONSO="doc/icons/os2/timed.ico doc/icons/os2/timed2.ico"
 
 mkdir -p dist
 rm -f dist/tim*.zip
-zip -jovX dist/tim112.zip $COMMON $ICONSW timed.exe fdbpack/fdbpack.exe doc/dos/file_id.diz
-zip -jovX dist/tim112x.zip $COMMON $ICONSW tim386.exe dos4gw.exe dos4gw.doc fdbpack/fdbpack.exe doc/d386/file_id.diz
-zip -jovX dist/tim112p.zip $COMMON $ICONSO timedp.exe fdbpack/fdbpackp.exe doc/os2/file_id.diz
-zip -jovX dist/tim112n.zip $COMMON $ICONSW timednt.exe fdbpack/fdbpackn.exe doc/nt/file_id.diz
+zip -jovX dist/tim112.zip $COMMON1 $COMMON2 $ICONSW timed.exe fdbpack/fdbpack.exe doc/dos/file_id.diz
+zip -jovX dist/tim112x.zip $COMMON1 $COMMON2 $ICONSW tim386.exe dos4gw.exe dos4gw.doc fdbpack/fdbpack.exe doc/d386/file_id.diz
+zip -jovX dist/tim112p.zip $COMMON1 $COMMON2 $ICONSO timedp.exe fdbpack/fdbpackp.exe doc/os2/file_id.diz
+zip -jovX dist/tim112n.zip $COMMON1 $COMMON2 $ICONSW timednt.exe fdbpack/fdbpackn.exe doc/nt/file_id.diz
