cf5b8b6bf83fc4533d07f06dee36b56f36040c82
Andrew Clarke <mail@ozzmosis.com>
2018-02-22 19:32:49 +1100

    Update dist script

--- timed/dist.sh
+++ timed/dist.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+DISTZIP="tim130"
+
 COMMON1="../COPYING doc/chrstuff.zip doc/advanced.cfg doc/timed.cfg doc/timkeys.cfg"
 COMMON2="doc/readmaps.dat doc/writmaps.dat doc/readme.txt doc/changes.txt doc/timed.txt"
 
@@ -7,8 +9,8 @@ ICONSW="doc/icons/windows/timed.ico doc/icons/windows/timed2.ico"
 ICONSO="doc/icons/os2/timed.ico doc/icons/os2/timed2.ico"
 
 mkdir -p dist
-rm -f dist/tim*.zip
-zip -jovX dist/tim112.zip $COMMON1 $COMMON2 $ICONSW timed.exe fdbpack/fdbpack.exe doc/dos/file_id.diz
-zip -jovX dist/tim112x.zip $COMMON1 $COMMON2 $ICONSW tim386.exe dos4gw.exe dos4gw.doc fdbpack/fdbpack.exe doc/d386/file_id.diz
-zip -jovX dist/tim112p.zip $COMMON1 $COMMON2 $ICONSO timedp.exe fdbpack/fdbpackp.exe doc/os2/file_id.diz
-zip -jovX dist/tim112n.zip $COMMON1 $COMMON2 $ICONSW timednt.exe fdbpack/fdbpackn.exe doc/nt/file_id.diz
+rm -f dist/$DISTZIP*.zip
+zip -jovX dist/$DISTZIP".zip" $COMMON1 $COMMON2 $ICONSW timed.exe fdbpack/fdbpack.exe doc/dos/file_id.diz
+zip -jovX dist/$DISTZIP"x.zip" $COMMON1 $COMMON2 $ICONSW tim386.exe dos4gw.exe dos4gw.doc fdbpack/fdbpack.exe doc/d386/file_id.diz
+zip -jovX dist/$DISTZIP"p.zip" $COMMON1 $COMMON2 $ICONSO timedp.exe fdbpack/fdbpackp.exe doc/os2/file_id.diz
+zip -jovX dist/$DISTZIP"n.zip" $COMMON1 $COMMON2 $ICONSW timednt.exe fdbpack/fdbpackn.exe doc/nt/file_id.diz
