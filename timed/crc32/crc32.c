8c345d02cce6e800722928292174119f47d9f62d
Andrew Clarke <mail@ozzmosis.com>
2018-02-22 18:26:53 +1100

    Add CRC32 calculator for config.c values

---
+++ timed/crc32/crc32.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <msgapi.h>
+#include <progprot.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+	
+	if (argc < 2)
+	{
+		fprintf(stderr, "Calculates CRC32 values as used in timEd config.c\n\n");
+		fprintf(stderr, "Usage: %s [keyword] [keyword] [...]\n", argv[0]);
+		return 1;
+	}
+	
+	for (i = 1; i < argc; i++)
+	{
+    	char buf[1024];
+		dword crc;
+
+		strcpy(buf, argv[i]);
+        strupr(buf);
+        crc = JAMsysCrc32(buf, strlen(buf), -1L);
+		printf("%-20s : 0x%08xL\n", buf, crc);
+	}
+	
+    return 0;
+}
