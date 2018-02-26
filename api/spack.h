5d8f9091ae7ae6b5d0d456d44876580889ae3b76
Andrew Clarke <mail@ozzmosis.com>
2018-02-26 18:19:47 +1100

    Add #pragma pack support files

---
+++ api/spack.h
@@ -0,0 +1,7 @@
+#ifdef __WATCOMC__
+#pragma pack(__push, 1)
+#endif
+
+#ifdef __GNUC__
+#pragma pack(push, 1)
+#endif
---
+++ api/spop.h
@@ -0,0 +1,7 @@
+#ifdef __WATCOMC__
+#pragma pack(__pop)
+#endif
+
+#ifdef __GNUC__
+#pragma pack(pop)
+#endif
