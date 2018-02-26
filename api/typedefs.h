b349c7a8203c0c5fa7a4438c21ed3410b380196b
Andrew Clarke <mail@ozzmosis.com>
2018-02-26 15:06:30 +1100

    Use C99 integer types for 64-bit compatibility.

--- api/typedefs.h
+++ api/typedefs.h
@@ -1,45 +1,17 @@
+#ifndef __TYPEDEFS_H__
+#define __TYPEDEFS_H__
 
-#ifndef __TYPEDEFS_H_DEFINED
-#define __TYPEDEFS_H_DEFINED
+#include <stdint.h>
 
-#if defined(__386__) || defined(__FLAT__) || defined(__GNUC__)
 typedef unsigned bit;
 
 typedef unsigned char byte;
 typedef signed char sbyte;
 
-typedef unsigned short word;
-typedef signed short sword;
+typedef uint16_t word;
+typedef int16_t sword;
 
-typedef unsigned int dword;
-typedef signed int sdword;
-
-#ifndef __GNUC__
-typedef unsigned short ushort;
-#endif
-typedef signed short sshort;
-
-#ifndef __GNUC__
-typedef unsigned long ulong;
-#endif
-typedef signed long slong;
-#else
-typedef unsigned bit;
-
-typedef unsigned char byte;
-typedef signed char sbyte;
-
-typedef unsigned int word;
-typedef signed int sword;
-
-typedef unsigned long dword;
-typedef signed long sdword;
-
-typedef unsigned short ushort;
-typedef signed short sshort;
-
-typedef unsigned long ulong;
-typedef signed long slong;
-#endif
+typedef uint32_t dword;
+typedef int32_t sdword;
 
 #endif
